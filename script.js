9a04d5be9181971a293f0a6c3f2c23185035333c
diyanshu0360 <diyanshu0360@gmail.com>
2023-04-29 14:49:18 +0530

    Javascript Strike, Remove Function added

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
+// adding task
 let addbtn = document.getElementById('inputbtn');
-
-addbtn.addEventListener('click', addText = () => {
+let addText = () => {
     const val = document.getElementById('inputtext');
     let txt = val.value;
-    addtodo(txt);
-    val.value = "";
-})
+    if(txt !== ""){
+        addtodo(txt);
+        val.value = "";
+    }
+}
 
-const addtodo = (txt) => {
+let addtodo = (txt) => {
     const text1 = document.createElement('span');
     text1.innerHTML = txt;
     const text2 = document.createElement('a');
-    text2.setAttribute('href', '#')
+    text2.setAttribute('href', '#');
     text2.innerHTML = 'delete';
     text2.classList.add('material-symbols-outlined', 'dustbin');
 
@@ -24,3 +26,24 @@ const addtodo = (txt) => {
     const showlist = document.getElementById('showlistid');
     showlist.appendChild(singlelistdiv);
 }
+
+
+addEventListener('click', (e) => {
+
+    // removing task 
+    if(e.target.classList.contains('dustbin')){
+        let rm = e.target.parentElement;
+        let rmp = rm.parentElement;
+        rmp.removeChild(rm);
+    }
+
+    // striking text
+    if(e.target.classList.contains('singlelist')){
+        let box = e.target;
+        let st = e.target.firstElementChild;
+        st.style.textDecoration = "line-through";
+        box.style.backgroundColor = 'red';
+        box.style.opacity = '50%';
+    }
+})
+
